0ab22c4ee57a983c09826030ff3f04d1fa4612cc
Patrick Li <baichuanli@yahoo.com>
2026-05-03 22:52:12 -0400

    fix: remove homebrew

--- .github/homebrew/unbill-cli/install.rb
+++
@@ -1,2 +0,0 @@
-bin.install "unbill-cli-macos-aarch64" => "unbill-cli" if Hardware::CPU.arm?
-bin.install "unbill-cli-linux-x86_64" => "unbill-cli" if OS.linux?
--- .github/homebrew/unbill-tui/install.rb
+++
@@ -1,2 +0,0 @@
-bin.install "unbill-tui-macos-aarch64" => "unbill-tui" if Hardware::CPU.arm?
-bin.install "unbill-tui-linux-x86_64" => "unbill-tui" if OS.linux?
